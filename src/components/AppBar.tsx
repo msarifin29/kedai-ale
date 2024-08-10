89ef4c17cb3c6956c5dd376f63c829b87fac204d
samsul <ms.arifin29@gmail.com>
2024-08-10 13:55:25 +0800

    initial

---
+++ src/components/AppBar.tsx
@@ -0,0 +1,26 @@
+
+import React from 'react';
+// import AppBar from '@mui/material/AppBar';
+// import Toolbar from '@mui/material/Toolbar';
+// import Typography from '@mui/material/Typography';
+// import Button from '@mui/material/Button';
+// import IconButton from '@mui/material/IconButton';
+// import { Box } from '@mui/material';
+// import { yellow } from '@mui/material/colors';
+
+const MyAppBar: React.FC = () => {
+    return (
+        <nav style={{
+            backgroundColor: "#ffee93",
+            color: 'black',
+            width: "100%",
+            height: '50px',
+            padding: '10px',
+            fontSize: 'bold'
+        }}>Catering</nav>
+    );
+};
+
+
+
+export default MyAppBar;
